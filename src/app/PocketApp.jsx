c806d094c37d61a4cdddfbd39404164480844f35
PARAPOPOVICH <139543918+KOSFin@users.noreply.github.com>
2025-06-21 00:41:55 +0500

    fix pocket adapt.

--- src/app/PocketApp.jsx
+++ src/app/PocketApp.jsx
@@ -1,5 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from '../pages/MainPage';
+
 const PocketApp = () => {
-    return;
+    return (
+        <Routes>
+            <Route path="/" element={<MainPage />} />
+        </Routes>
+    )
 }
 
 export default PocketApp;
