b5194b200d065688d0d8d253cbeda53090f9a534
PARAPOPOVICH <139543918+KOSFin@users.noreply.github.com>
2025-07-03 00:23:43 +0500

    fix deploy error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from 'vite-plugin-pwa'
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
-
 export default defineConfig({
   plugins: [
     react(),
